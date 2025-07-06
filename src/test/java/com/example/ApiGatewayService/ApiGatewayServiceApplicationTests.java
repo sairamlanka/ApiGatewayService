dc00a935a00af40377f9c6f55e9058b5e2c596dc
sairamlanka <sairam8458@gmail.com>
2025-07-06 20:46:28 +0530

    Eureke dependancy commented

--- src/test/java/com/example/ApiGatewayService/ApiGatewayServiceApplicationTests.java
+++ src/test/java/com/example/ApiGatewayService/ApiGatewayServiceApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.ApiGatewayService;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ImportAutoConfiguration(exclude = {org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration.class})
 class ApiGatewayServiceApplicationTests {
 
 	@Test
